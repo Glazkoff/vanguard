56edcd59367846221bba418c722d4b18419842be
nvkolezneva <nvkolezneva@gmail.com>
2021-01-20 13:32:44 +0300

    dev: search,import,export and partly filter in models employee and employeeinorganization

--- employees/admin.py
+++ employees/admin.py
@@ -1,16 +1,49 @@
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from django.contrib import admin
 from .models import Employee, EmployeeInOrganization
 
+class EmployeeResource(resources.ModelResource):
+    """Ресурс сотрудника для импорта"""
+    class Meta:
+        model = Employee
 
-class EmployeeAdmin(admin.ModelAdmin):
+class EmployeeAdmin(ImportExportModelAdmin):
     """Работники"""
     exclude = ('createdAt', 'updatedAt')
+    # list_display=('')
+    list_filter = ('birthday','citizenship','registrationValidityPeriod','dateOfNotificationMVDadmission','dateOfNotificationMVDdischarge')
+    search_fields=('fullName','fullNameInGenetive')
+    fieldsets = (
+        (None, {
+            'fields': (('fullName','fullNameInGenetive'), 'birthday','phoneNumber',('INN','SNILS'),'bankDetails')
+        }),
+        ('Данные паспорта', {
+            'fields': (('passportNumber','passportValidityPeriod'),'citizenship', 'passportIssuedBy')
+        }),
+        ('Данные о регистрации', {
+            'fields': ( 'registrationValidityPeriod','registrationAddress')
+        }),
+        ('Данные из МВД', {
+            'fields': ('dateOfNotificationMVDadmission', 'dateOfNotificationMVDdischarge')
+        }),
+    )
+    resource_class = EmployeeResource
 
+class EmployeeInOrganizationResource(resources.ModelResource):
+    """Ресурс сотрудника в организации для импорта"""
+    class Meta:
+        model = EmployeeInOrganization
 
-class EmployeeInOrganizationAdmin(admin.ModelAdmin):
+class EmployeeInOrganizationAdmin(ImportExportModelAdmin):
     """Работники, прикрепленные к организациям"""
     exclude = ('createdAt', 'updatedAt')
+    list_filter = ('tariff','organization__organizationName')
+    search_fields=('tariff__positionName','organization__organizationName')
+    resource_class = EmployeeInOrganizationResource
 
 
 admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(EmployeeInOrganization, EmployeeInOrganizationAdmin)
+admin.site.site_title = "Авангард"
+admin.site.site_header = "Авангард"
