f59d309271f5a233147da096b48bf99f0ac8561b
Darya Belyaeva <d.belyaeva1@gmail.com>
2021-06-07 22:40:56 +0300

    DEV: update of all models is ready

--- backend/employees/admin.py
+++ backend/employees/admin.py
@@ -11,15 +11,15 @@ class EmployeeResource(resources.ModelResource):
 class EmployeeAdmin(ImportExportModelAdmin):
     """Работники"""
     exclude = ('createdAt', 'updatedAt')
-    # list_display=('')
-    list_filter = ('birthday','citizenship','registrationValidityPeriod','dateOfNotificationUFMSadmission','dateOfNotificationUFMSdischarge', 'endDateOfResidencePermit', 'endDateOfRVP')
+    # list_display=('',)
+    list_filter = ('birthday','citizenship','registrationValidityPeriod','dateOfNotificationUFMSadmission','dateOfNotificationUFMSdischarge', 'endDateOfResidencePermit', 'endDateOfRVP', 'passportIssueDate')
     search_fields=('fullName','fullNameInGenetive')
     fieldsets = (
         (None, {
-            'fields': (('fullName','fullNameInGenetive'), 'birthday','phoneNumber',('INN','SNILS'),'bankDetails', ('endDateOfResidencePermit', 'endDateOfRVP'))
+            'fields': (('fullName','fullNameInGenetive'), 'birthday', 'birthplace', 'phoneNumber',('INN','SNILS'),'bankDetailsCardNumber', ('endDateOfResidencePermit', 'endDateOfRVP'))
         }),
         ('Данные паспорта', {
-            'fields': (('passportNumber','passportValidityPeriod'),'citizenship', 'passportIssuedBy')
+            'fields': (('passportNumber','passportValidityPeriod'),'citizenship', 'passportIssuedBy', 'passportIssueDate')
         }),
         ('Данные о регистрации', {
             'fields': ( 'registrationValidityPeriod','registrationAddress')
@@ -29,10 +29,6 @@ class EmployeeAdmin(ImportExportModelAdmin):
         }),
     )
     resource_class = EmployeeResource
-
-    def get_queryset(self, request):
-        qs = super(EmployeeAdmin, self).get_queryset(request)
-        return qs.filter(deleted=False)
 
 
 class EmployeeInOrganizationResource(resources.ModelResource):
--- backend/organizations/models.py
+++ backend/organizations/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 from django.core.validators import int_list_validator
 
+class City(models.Model):
+    """Город"""
+    cityName = models.TextField("Название города")
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cityName
+
+    class Meta:
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
+
 
 class Organization(models.Model):
     """Организация"""
     organizationName = models.TextField("Название организации")
     legalOrganizationAddress = models.TextField(
         "Юридический адрес организации")
-    cityOrganization = models.TextField(
-        "Город организации")
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name="Город организации")
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField("Удалено", default=False)
-
-
+   
     def __str__(self):
         return self.organizationName
 
@@ -24,14 +35,15 @@ class Organization(models.Model):
 
 class Tariff(models.Model):
     """Тариф"""
-    organization = models.ForeignKey(
-        Organization, on_delete=models.CASCADE, verbose_name="Название организации")
+    PLACE_CHOICES = (('kitchen', 'Место на кухне'),('hall', 'Место в зале'))
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, verbose_name="Город организации")
+
     positionName = models.CharField("Название должности", max_length=150)
     salaryPerHour = models.PositiveIntegerField("Заработная плата в час", validators=[int_list_validator( sep = '' , message = "ИНН должен содержать только цифры" , code = 'invalid' , allow_negative = False )])
+    kitchenOrHall = models.CharField("Область работы (кухня, зал)", max_length=15, choices=PLACE_CHOICES)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField("Удалено", default=False)
-
 
     def __str__(self):
         return f"{self.positionName} ({self.salaryPerHour}₽ в час)"
