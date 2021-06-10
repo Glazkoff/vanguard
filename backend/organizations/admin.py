424870ceb55b930693647564caa84f364843a851
Nikita Glazkov <zitrnik@gmail.com>
2021-06-11 00:17:09 +0300

    DEV: add admin settings for filtering

--- backend/organizations/admin.py
+++ backend/organizations/admin.py
@@ -6,50 +6,55 @@ from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from import_export.formats import base_formats
 
 
-class CityResource(resources.ModelResource): 
+class CityResource(resources.ModelResource):
     """Ресурс для импорта-экспорта городов"""
     class Meta:
         model = City
+
 
 class CityAdmin(ImportExportModelAdmin):
     """Города"""
     list_display = ("cityName",)
     search_fields = ("cityName",)
-    
 
-class OrganizationResource(resources.ModelResource): 
+
+class OrganizationResource(resources.ModelResource):
     """Ресурс для импорта-экспорта организации"""
     class Meta:
         model = Organization
+
 
 class OrganizationAdmin(ImportExportModelAdmin):
     """Организации"""
     list_display = ("organizationName", "legalOrganizationAddress", "city")
     list_display_links = ("organizationName",)
+    list_filter = ("city",)
     search_fields = ("organizationName",)
     ordering = ['-organizationName']
-    
+
     def get_export_formats(self):
-            formats = (
-                  base_formats.XLS,
-                  base_formats.XLSX,
-            )
-            return [f for f in formats if f().can_export()]
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
+
     def get_import_formats(self):
-            formats = (
-                  base_formats.XLS,
-                  base_formats.XLSX,
-            )
-            return [f for f in formats if f().can_import()]
-    def get_queryset(self, request):
-        qs = super(OrganizationAdmin, self).get_queryset(request)
-        return qs.filter(deleted=False)
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+#     def get_queryset(self, request):
+#         qs = super(OrganizationAdmin, self).get_queryset(request)
+#         return qs.filter(deleted=False)
 
 
-class TariffResource(resources.ModelResource): 
+class TariffResource(resources.ModelResource):
     """Ресурс для импорта-экспорта патентов"""
     class Meta:
         model = Tariff
+
 
 class TariffAdmin(ImportExportModelAdmin):
     """Тарифы"""
@@ -57,22 +62,23 @@ class TariffAdmin(ImportExportModelAdmin):
     list_display_links = ("positionName",)
     search_fields = ("positionName", "salaryPerHour",)
     ordering = ['-salaryPerHour']
-    
+
     def get_export_formats(self):
-            formats = (
-                  base_formats.XLS,
-                  base_formats.XLSX,
-            )
-            return [f for f in formats if f().can_export()]
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
+
     def get_import_formats(self):
-            formats = (
-                  base_formats.XLS,
-                  base_formats.XLSX,
-            )
-            return [f for f in formats if f().can_import()]
-    def get_queryset(self, request):
-        qs = super(TariffAdmin, self).get_queryset(request)
-        return qs.filter(deleted=False)
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+#     def get_queryset(self, request):
+#         qs = super(TariffAdmin, self).get_queryset(request)
+#         return qs.filter(deleted=False)
 
 
 admin.site.register(City, CityAdmin)
