36533dc3c7c153ab93117c6c41720cecfdcb1793
Nikita Glazkov <zitrnik@gmail.com>
2021-07-19 18:59:56 +0300

    fix: gunicorn config

--- backend/scripts/gunicorn.sh
+++ backend/scripts/gunicorn.sh
@@ -6,4 +6,4 @@ set -o nounset
 
 python manage.py migrate
 python manage.py collectstatic --noinput --verbosity 0
-gunicorn config.wsgi -w 4 --worker-class gevent -b 0.0.0.0:8002 --chdir=/app
+gunicorn vanguard.wsgi -w 4 --worker-class gevent -b 0.0.0.0:8002 --chdir=/app
