b6f6ba73ed600bd5eb45b6f49dd7150691afec72
Nikita Glazkov <zitrnik@gmail.com>
2021-07-19 15:46:22 +0300

    FIX: fix notifications on admin index page

--- backend/employees/notifications.py
+++ backend/employees/notifications.py
@@ -1,4 +1,3 @@
-import admin_notifications
 from patents.models import PatentPaymentReceipt, Patent
 from datetime import date, timedelta
 from django.utils.html import format_html
@@ -17,21 +16,14 @@ def go_through_patents():
                 patentExpirency, 'd E Y г.')
             d = date.today()+timedelta(days=14)
             if patentExpirency <= d:
-                notifications.append(patent.employee)
-
-                def notification():
-                    _ = patent.employee
-                    print("!!!")
-                    print(_)
-                    return '<a href="/admin/patents/patent/'+str(patent.id)+'/change/">Патент #'+str(_.id)+'</a> сотрудника <a href="/admin/employees/employee/'+str(_.id)+'/change/">'+_.fullNameInGenetive+'</a> оплачен только до '+formatPatentExpirency+"!"
-                admin_notifications.register(notification)
+                _ = patent.employee
+                notifications.append('<a href="/admin/patents/patent/'+str(patent.id)+'/change/">Патент #'+str(
+                    _.id)+'</a> сотрудника <a href="/admin/employees/employee/'+str(_.id)+'/change/">'+_.fullNameInGenetive+'</a> оплачен только до '+formatPatentExpirency+"!")
     return notifications
 
 
-# notif_list = go_through_patents()
-
-
-# for _ in notif_list:
-# def notification():
-#     return 'Патент #'+str(_.id)+' сотрудника <a href="/admin/employees/employee/'+str(_.id)+'/change/">'+_.fullNameInGenetive+'</a> оплачен только до'
-# admin_notifications.register(notification)
+def context_processor(request):
+    extra_context = {}
+    if request.path == '/admin/':
+        extra_context['notifications'] = go_through_patents()
+    return extra_context
