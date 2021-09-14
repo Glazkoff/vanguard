56e6aa3d2acf826987909421759bba4ed8ba3cdb
nvkolezneva <nvkolezneva@gmail.com>
2021-09-14 23:41:29 +0300

    FIX:delete test documents and yellow selection in documents

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -1,9 +1,9 @@
-from employees.views import doc_test, doc_multiple_test, labor_contract, gph_contract, mia_notifications_admission,mia_notification_discharge
+from employees.views import labor_contract, gph_contract, mia_notifications_admission,mia_notification_discharge
 from django.urls import path
 
 urlpatterns = [
-    path('documents/', doc_test),
-    path('documents_m/', doc_multiple_test),
+#     path('documents/', doc_test),
+#     path('documents_m/', doc_multiple_test),
     path('documents/labor_contract/<int:employee_in_org_id>/', labor_contract),
     path('documents/gph_contract/<int:employee_in_org_id>/', gph_contract),
     path('documents/mia_notifications_admission/<int:employee_in_org_id>/',
--- backend/employees/views.py
+++ backend/employees/views.py
@@ -18,57 +18,57 @@ from patents.models import Patent
 APP_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-@login_required(login_url='/admin')
-def doc_test(request):
-    doc = DocxTemplate(os.path.join(
-        APP_ROOT, "docs", "test_1.docx"))
-    # ... your other code ...
-    context = {'title': "NGLAZKOV one file"}
-    doc.render(context)
-    doc_io = io.BytesIO()  # create a file-like object
-    doc.save(doc_io)  # save data to file-like object
-    doc_io.seek(0)  # go to the beginning of the file-like object
+# @login_required(login_url='/admin')
+# def doc_test(request):
+#     doc = DocxTemplate(os.path.join(
+#         APP_ROOT, "docs", "test_1.docx"))
+#     # ... your other code ...
+#     context = {'title': "NGLAZKOV one file"}
+#     doc.render(context)
+#     doc_io = io.BytesIO()  # create a file-like object
+#     doc.save(doc_io)  # save data to file-like object
+#     doc_io.seek(0)  # go to the beginning of the file-like object
 
-    response = HttpResponse(doc_io.read())
+#     response = HttpResponse(doc_io.read())
 
-    # Content-Disposition header makes a file downloadable
-    response["Content-Disposition"] = "attachment; filename=generated_doc.docx"
+#     # Content-Disposition header makes a file downloadable
+#     response["Content-Disposition"] = "attachment; filename=generated_doc.docx"
 
-    # Set the appropriate Content-Type for docx file
-    response["Content-Type"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+#     # Set the appropriate Content-Type for docx file
+#     response["Content-Type"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
 
-    return response
-
-
-def generate_zip(files):
-    mem_zip = io.BytesIO()
-
-    with zipfile.ZipFile(mem_zip, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
-        for f in files:
-            zf.writestr(f[0], f[1])
-
-    return mem_zip.getvalue()
+#     return response
 
 
-@login_required(login_url='/admin')
-def doc_multiple_test(request):
-    files = []
+# def generate_zip(files):
+#     mem_zip = io.BytesIO()
 
-    for i in range(5):
-        doc = DocxTemplate(os.path.join(APP_ROOT, "docs", "test_1.docx"))
-        # ... your other code ...
-        context = {'title': "NGLAZKOV company - multiple " + str(i) + " tests"}
-        doc.render(context)
-        doc_io = io.BytesIO()  # create a file-like object
-        doc.save(doc_io)  # save data to file-like object
-        doc_io.seek(0)  # go to the beginning of the file-like object
-        files.append(("test"+str(i)+".docx", doc_io.getvalue()))
+#     with zipfile.ZipFile(mem_zip, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
+#         for f in files:
+#             zf.writestr(f[0], f[1])
 
-    full_zip_in_memory = generate_zip(files)
+#     return mem_zip.getvalue()
 
-    response = HttpResponse(full_zip_in_memory, content_type='application/zip')
-    response['Content-Disposition'] = 'attachment; filename=test.zip'
-    return response
+
+# @login_required(login_url='/admin')
+# def doc_multiple_test(request):
+#     files = []
+
+#     for i in range(5):
+#         doc = DocxTemplate(os.path.join(APP_ROOT, "docs", "test_1.docx"))
+#         # ... your other code ...
+#         context = {'title': "NGLAZKOV company - multiple " + str(i) + " tests"}
+#         doc.render(context)
+#         doc_io = io.BytesIO()  # create a file-like object
+#         doc.save(doc_io)  # save data to file-like object
+#         doc_io.seek(0)  # go to the beginning of the file-like object
+#         files.append(("test"+str(i)+".docx", doc_io.getvalue()))
+
+#     full_zip_in_memory = generate_zip(files)
+
+#     response = HttpResponse(full_zip_in_memory, content_type='application/zip')
+#     response['Content-Disposition'] = 'attachment; filename=test.zip'
+#     return response
 
 
 @login_required(login_url='/admin')
